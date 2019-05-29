344abfa4a76dc188c7b800040d611caa11c49bf0
li <mr_lgm@outlook.com>
2019-05-29 10:47:22 +0800

    run

--- run.py
+++ run.py
@@ -1,45 +1,103 @@
+import wiringpi
+import pygame
 import cv2 as cv
 import keras
 import numpy as np 
 import time 
 
-images = []
-angles = []
-for i in range(4):
-    name = input("input image name:")
-    img_path = './test_image/' + name + '.jpeg'
-    angle = int(img_path.split('_')[2].split('.')[0])
-    angle = 620 - int(angle*240/1079) - 381
-    print(angle)
-    print('path:' + img_path)
-    images.append(img_path)
-    angles.append(angle)
+def gpioInit():
+    wiringpi.wiringPiSetup()
+    wiringpi.pinMode(1, wiringpi.PWM_OUTPUT)
+    wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
+    wiringpi.pwmSetRange(12000)
+    wiringpi.pwmSetClock(32)
+    wiringpi.pinMode(22, wiringpi.SOFT_PWM_OUTPUT)
+    wiringpi.pinMode(23, wiringpi.SOFT_PWM_OUTPUT)
+    wiringpi.pinMode(24, wiringpi.SOFT_PWM_OUTPUT)
+    wiringpi.pinMode(25, wiringpi.SOFT_PWM_OUTPUT)
+    wiringpi.softPwmCreate(22, 0, 100)
+    wiringpi.softPwmCreate(23, 0, 100)
+    wiringpi.softPwmCreate(24, 0, 100)
+    wiringpi.softPwmCreate(25, 0, 100)
 
-model = keras.models.load_model('model.h5')
 
-for i in range(4):
-    start = time.clock()
-    img_data = []
-    img = cv.imread(images[i], 0)
-    img_show = img 
-    img = cv.resize(img, (240, 60))
-    img = img.tolist()
-    img_data.append(img)
-    img_data = np.array(img_data, dtype=np.uint8)
-    img_data = img_data.reshape(img_data.shape[0], img_data.shape[1], img_data.shape[2], 1).astype('float32')
-    img_data = img_data / 255
-    
-    prediction = model.predict(img_data)
-    pred = 240 * prediction
-    end = time.clock()
-    print('time:' + str(end-start))
-    print(pred, angles[i])
-    loss = abs(angles[i] - pred[0]) / 240 * 100
-    print(loss)
-    loss = ('%.2f'%loss)
-    string = 'act:' + str(angles[i]) + ', ' + 'pre:' + str(pred[0])
-    cv.putText(img_show, string, (10,20), cv.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
-    cv.putText(img_show, 'loss:' + str(loss) + '%', (10,40), cv.FONT_HERSHEY_PLAIN, 1, (255,255,255))
-    cv.imshow("img", img_show)
-    cv.waitKey(0)
-cv.destroyAllWindows()
+def setServoVal(val):
+    if val < 380:
+        val = 380
+    if val > 620:
+        val = 620
+    wiringpi.pwmWrite(1, val)
+
+
+def setMotorVal(val):
+    if val >= 0:
+        wiringpi.softPwmWrite(22, val)
+        wiringpi.softPwmWrite(23, 0)
+        wiringpi.softPwmWrite(24, 0)
+        wiringpi.softPwmWrite(25, val)
+    else:
+        wiringpi.softPwmWrite(22, 0)
+        wiringpi.softPwmWrite(23, val)
+        wiringpi.softPwmWrite(24, val)
+        wiringpi.softPwmWrite(25, 0)
+
+
+def main():
+    start_flag = 0
+    speed = 4
+    index = 0
+    pygame.init()
+    gpioInit()
+    capture = cv.VideoCapture(0)
+    capture.set(cv.CAP_PROP_FRAME_WIDTH, 256)
+    capture.set(cv.CAP_PROP_FRAME_HEIGHT, 128)
+    screen = pygame.display.set_mode((0, 0), 0, 32)
+    screen_rect = screen.get_rect()
+    model = keras.models.load_model('model.h5')
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    print("exit")
+                    cv.destroyAllWindows()
+                    exit()
+                elif event.key == pygame.K_w:
+                    speed = speed + 1
+                elif event.key == pygame.K_s:
+                    speed = speed - 1
+                else:
+                    pass
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                start_flag = ~start_flag
+
+            if start_flag:
+                setMotorVal(speed)
+            else:
+                setMotorVal(0)
+            print(speed)
+        
+        start = time.clock()
+        ret, frame = capture.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        img = cv.resize(gray, (240, 60))
+        img_data = []
+        img = img.tolist()
+        img_data.append(img)
+        img_data = np.array(img_data, dtype=np.uint8)
+        img_data = img_data.reshape(img_data.shape[0], img_data.shape[1], img_data.shape[2], 1).astype('float32')
+        img_data = img_data / 255
+        
+        prediction = model.predict(img_data)
+        pred = int(240 * prediction[0] + 381)
+        end = time.clock()
+        print('time:' + str(end-start))
+        setServoVal(pred)
+
+        string = 'pre:' + str(pred)
+        cv.putText(frame, string, (10,20), cv.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
+        cv.imshow("img", frame)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_image.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+import keras
+import numpy as np 
+import time 
+
+images = []
+angles = []
+for i in range(4):
+    name = input("input image name:")
+    img_path = './test_image/' + name + '.jpeg'
+    angle = int(img_path.split('_')[2].split('.')[0])
+    angle = 620 - int(angle*240/1079) - 381
+    print(angle)
+    print('path:' + img_path)
+    images.append(img_path)
+    angles.append(angle)
+
+model = keras.models.load_model('model.h5')
+
+for i in range(4):
+    start = time.clock()
+    img_data = []
+    img = cv.imread(images[i], 0)
+    img_show = img 
+    img = cv.resize(img, (240, 60))
+    img = img.tolist()
+    img_data.append(img)
+    img_data = np.array(img_data, dtype=np.uint8)
+    img_data = img_data.reshape(img_data.shape[0], img_data.shape[1], img_data.shape[2], 1).astype('float32')
+    img_data = img_data / 255
+    
+    prediction = model.predict(img_data)
+    pred = 240 * prediction
+    end = time.clock()
+    print('time:' + str(end-start))
+    print(pred, angles[i])
+    loss = abs(angles[i] - pred[0]) / 240 * 100
+    print(loss)
+    loss = ('%.2f'%loss)
+    string = 'act:' + str(angles[i]) + ', ' + 'pre:' + str(pred[0])
+    cv.putText(img_show, string, (10,20), cv.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
+    cv.putText(img_show, 'loss:' + str(loss) + '%', (10,40), cv.FONT_HERSHEY_PLAIN, 1, (255,255,255))
+    cv.imshow("img", img_show)
+    cv.waitKey(0)
+cv.destroyAllWindows()
